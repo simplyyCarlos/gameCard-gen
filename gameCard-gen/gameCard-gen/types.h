2f703d9b694e9c4e4e4a5059f032610ab7516105
ElPountie <khalis.rabehi@etu.univ-lyon1.fr>
2023-04-04 15:37:40 +0200

    fix : add types of monster in magic card

--- gameCard-gen/gameCard-gen/types.h
+++ gameCard-gen/gameCard-gen/types.h
@@ -24,3 +24,29 @@ enum typesMagic
 {
 	Rituel,Ephemere,Enchantement,Artefact,Creature,Planeswalker,Terrain
 };
+
+enum typesCreatureMagic
+{
+	Allie, Ange, Annonciateur, Anomalie, Antilope, Apparition, Araignee, Archer, Archonte, Armee, 
+	Artificier, Assassin, Atog, Aurochs, Avatar, Azra, Baleine, Barbare, Barde, Basilic, Berserker, Bête, 
+	Bibule, Blaireau, bœuf, Bosquerisson, Calamar, Camaride, Caribou, Cauchemar, Centaure, Cephalide, chacal, 
+	Chameau, Changeforme, Chassenuit, Chat, Chauve_souris, Cheval, Chevalier, Chevre, Chien, Chimere, Citoyen, 
+	Clerc, Cocatrix, Conseiller, Construction, convertisseur, Couard, Crabe, Crocodile, Cuirasse, Cyclope, Dauthi, 
+	demidieu, Demon, Desencave, Deserteur, Diable, Diablotin, Dieu, dinosaure, Djaggernaut, Djinn, doyen, Dragon, 
+	Drakôn, Drone, Druide, Dryade, Eclaireur, Ecureuil, Efrit, Elan, Eldrazi, Elemental, Elephant, Elfe, encrelin, 
+	Engeance, Enseigne, Epix, Eponge, Epouvantail, Esprit, Etherien, Etoile_de_mer, Evolien, exfoliant, Fongus, 
+	fractale, Furet, Gargouille, Geant, Germe, gith, Glouton, Gnoll, Gnome, Gobelin, Golem, Gorgonoïde, Grand_serpent, 
+	Grand_singe, Gredin, Greeur, Gremlin, Grenouille, Griffon, Guerrier, Guivre, Halfelin, Hamster, Harpie, Hippogriffe, 
+	Hippopotame, Homaride, Homoncule, Horreur, Huître, Humain, Hydre, Hyene, Illusion, Incarnation, Insecte, Kavru, 
+	Kirin, Kobold, Kor, Kraken, Lamassu, lamie, Lapin, Leviathan, Lezard, Lhurgoyf, Licorne, Limace, Limon, Litique, 
+	Loup, Loup_garou, loutre, Lunareen, Mangouste, Manticore, Masticore, Mecanoptere, Meduse, Megere, Mercenaire, 
+	Metathran, Mignon, Minotaure, Moine, Monstruosite, morse, Mouton, Mur, Mutant, Myr, Mystique, naga, Nain, Nautile, 
+	Negociant, Nephilim, Neugle, Ninja, noble, Nomade, Nymphe, œil, œuf, Ogre, Oiseau, Ombre, Ondin, Orbe, Orgg, Orphe, 
+	Orque, Ours, Ouvrier_specialise, pangolin, Parasite, paysan, Pegase, Pentavite, Peuple_fee, Phelddagrif, Phenix, 
+	phyrexian, Pieuvre, pilote, Pinceur, Pirate, Plante, Praetor, Poisson, Prisme, Propagateur, Psychagogue, Ranger, Rat, 
+	Rebelle, reflexion, Renard, requin, Rhinoceros, Sable, Salamandre, Samouraï, Sangami, Sanglier, Sangsue, Saprobionte,
+	Satyre, Scintimite, scion, Scorpion, sculpture, Serf, servo, Shamane, singe, Sirene, Slith, Slivoïde, Soldat, Soltari, 
+	Sorcier, Sortisan, souris, Spectre, Sphinx, Squelette, Srâne, Surrakar, Survivant, Sylvin, taupe, tentacule, Tetravite, 
+	Thalakos, Tieffelin, tortue, trilobite, Triskelavite, Troll, Tyrannoeil, Vampire, Vedalken, Ver, Viashino, Wombat, Yeti,
+	Zibeline, Zombie,Zubera
+};
